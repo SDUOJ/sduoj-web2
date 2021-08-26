43f9a14f9d52bee55ed617e17903f50c78290f1d
yhf2000 <735961159@qq.com>
2021-08-26 15:28:41 +0800

    Merge remote-tracking branch 'origin/master'

--- src/Component/user/UserList.tsx
+++ src/Component/user/UserList.tsx
@@ -2,11 +2,11 @@ import React, {Component} from "react";
 
 import {IUser, IUserPropRoles, Sex} from '../../Type/Iuser'
 import {Button, Space, Table, TablePaginationConfig} from "antd";
-import DeleteUser from "./DeleteUser";
 import {withTranslation} from "react-i18next";
 import {RouteComponentProps, withRouter} from "react-router-dom";
 import {FilterValue, SorterResult, TableCurrentDataSource} from "antd/lib/table/interface";
-import {ManOutlined, WomanOutlined, QuestionOutlined} from "@ant-design/icons"
+import {ManOutlined, QuestionOutlined, WomanOutlined} from "@ant-design/icons"
+import DeleteUser from "./DeleteUser";
 
 
 interface IUserListState {
@@ -16,50 +16,50 @@ interface IUserListState {
     showCol: object
 }
 
-interface IUserListCol{
+interface IUserListCol {
     title_i18n: string
     dataIndex: string,
     render: any
 }
 
-const colData:IUserListCol[] = [
+const colData: IUserListCol[] = [
     {
         title_i18n: "#",
-        dataIndex:"id",
+        dataIndex: "id",
         render: null
     },
     {
         title_i18n: "username",
-        dataIndex:"username",
+        dataIndex: "username",
         render: null
     },
     {
         title_i18n: "nickname",
-        dataIndex:"nickname",
+        dataIndex: "nickname",
         render: null
     },
     {
         title_i18n: "sex",
-        dataIndex:"sex",
-        render: (sex:Sex) =>{
-            switch (sex){
+        dataIndex: "sex",
+        render: (sex: Sex) => {
+            switch (sex) {
                 case Sex.Male:
-                    return <ManOutlined />
+                    return <ManOutlined/>
                 case Sex.Female:
-                    return <WomanOutlined />
+                    return <WomanOutlined/>
                 case Sex.Unknown:
-                    return <QuestionOutlined />
+                    return <QuestionOutlined/>
             }
         }
     },
     {
-        title_i18n: "#",
-        dataIndex:"id",
+        title_i18n: "operator",
+        dataIndex: "id",
         render: null
     },
     {
         title_i18n: "#",
-        dataIndex:"id",
+        dataIndex: "id",
         render: null
     },
 ]
@@ -75,6 +75,7 @@ class UserList extends Component<IUserPropRoles & RouteComponentProps, IUserList
                 id: i,
                 username: i.toString(),
                 nickname: i.toString(),
+                sex: (i%3),
                 roles: []
             })
         }
@@ -122,32 +123,29 @@ class UserList extends Component<IUserPropRoles & RouteComponentProps, IUserList
 
                 >
                     {
-
+                        colData.map((r, i) => {
+                            return (
+                                <Table.Column
+                                    title={this.props.t(r.title_i18n)}
+                                    dataIndex={r.dataIndex}
+                                    render={r.render}
+                                />
+                            )
+                        })
+                    }
+                    {
                         (
                             <Table.Column
-                                title={"#"}
-                                dataIndex={'id'}
-                            />
+                                title={this.props.t("operator")}
+                                render={(user: IUser) => (
+                                    <Space>
+                                        <Button type='primary'>编辑</Button>
+                                        <DeleteUser callback={this.deleteUser} id={user.id}/>
+                                    </Space>
+                                )}/>
                         )
-
                     }
 
-                    <Table.Column
-                        title={"Username"}
-                        dataIndex={'username'}
-                    />
-                    <Table.Column
-                        title={"Username"}
-                        dataIndex={'username'}
-                    />
-                    <Table.Column
-                        title={""}
-                        render={(user: IUser) => (
-                            <Space>
-                                <Button type='primary'>编辑</Button>
-                                <DeleteUser callback={this.deleteUser} id={user.id}/>
-                            </Space>
-                        )}/>
                 </Table>
             </>
         )
