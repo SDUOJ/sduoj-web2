6bb2ef3c7895d4c74e9cd911a26fe867da65e565
yhf2000 <735961159@qq.com>
2021-12-19 21:01:30 +0800

    修复显示 bug

--- src/Component/submission/Processing.tsx
+++ src/Component/submission/Processing.tsx
@@ -310,7 +310,8 @@ class Processing extends Component<IProcessingProp & any, SProcessing> {
                             </div>
                             <div style={{marginTop: "30px"}}>
                                 {
-                                    (this.state.RunningState === "-2" || this.state.RunningState === "-1") && (
+                                    (this.state.RunningState === "-2" || this.state.RunningState === "-1") &&
+                                    (this.state.RunningResult !== "8" && this.state.RunningResult !== "5") && (
                                         [''].map(() => {
                                             return (
                                                 <>
