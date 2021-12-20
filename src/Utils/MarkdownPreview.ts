fb2c3728431c0cae487ab8da26c1a7ebb3d4c9c1
yhf2000 <735961159@qq.com>
2021-12-20 12:28:33 +0800

    调整 markdown 编辑器的资源文件到本地

--- src/Utils/MarkdownPreview.ts
+++ src/Utils/MarkdownPreview.ts
@@ -6,13 +6,20 @@ export function MarkdownPreview(id: string, code: string) {
     VditorPreview.preview(
         document.getElementById(id),
         code,
-        process.env.NODE_ENV === 'development' ? {} :
-        {
-            cdn: "https://oj.qd.sdu.edu.cn/vditor",
-            emojiPath: "https://oj.qd.sdu.edu.cn/vditor/dist/images/emoji",
-            theme: {
-                path: "https://oj.qd.sdu.edu.cn/vditor/dist/css/content-theme"
+        process.env.NODE_ENV === 'development' ?
+            {
+                cdn: "http://oj.cs.sdu.edu.cn:3000/vditor",
+                emojiPath: "http://oj.cs.sdu.edu.cn:3000/vditor/dist/images/emoji",
+                theme: {
+                    path: "http://oj.cs.sdu.edu.cn:3000/vditor/dist/css/content-theme"
+                }
+            } :
+            {
+                cdn: "https://oj.qd.sdu.edu.cn/vditor",
+                emojiPath: "https://oj.qd.sdu.edu.cn/vditor/dist/images/emoji",
+                theme: {
+                    path: "https://oj.qd.sdu.edu.cn/vditor/dist/css/content-theme"
+                }
             }
-        }
     )
 }
