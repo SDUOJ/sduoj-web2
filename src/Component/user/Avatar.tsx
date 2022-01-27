803afaa570984733e2edce83d612fb6697517a31
yhf2000 <735961159@qq.com>
2022-01-27 17:07:45 +0800

    update: 更改公共头像库为 cravatar

--- src/Component/user/Avatar.tsx
+++ src/Component/user/Avatar.tsx
@@ -2,18 +2,24 @@ import {UserOutlined} from "@ant-design/icons";
 import md5 from "js-md5";
 import React from "react";
 import {Avatar} from "antd";
+import {AvatarSize} from "antd/es/avatar/SizeContext";
 
 
 interface IUserAvatar {
     email?: string | null
+    size?: AvatarSize
 }
 
 const UserAvatar = (props: IUserAvatar) => {
     if (props.email !== undefined && props.email !== null) {
         return (
             <Avatar
-                src={"https://sdn.geekzu.org/avatar/" + md5(props.email)}
+                src={"https://cravatar.cn/avatar/" + md5(props.email) +
+                (typeof props.size === "number" ? "?s=" + props.size : "") +
+                (typeof props.size === "number" ? "&d=identicon": "?d=identicon")
+                }
                 alt={"头像"}
+                size={props.size}
             />
         )
     } else {
