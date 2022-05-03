fe2ad697945c67a63ca4b92b909ba4def1ead447
yhf2000 <735961159@qq.com>
2022-05-03 13:49:18 +0800

    fix: #23

--- src/Component/problem/Program/ProProgramDetail.tsx
+++ src/Component/problem/Program/ProProgramDetail.tsx
@@ -8,7 +8,6 @@ import {connect} from "react-redux";
 const ProProgramDetail = (props: any) => {
 
     const problemInfo = props.problemInfo
-
     return (
         <>
             <Card title={"题目详情"} className={"smallBodyPaddingTop"}>
