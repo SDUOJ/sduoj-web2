d01e5e05f8a0c590412dde4995d91b3e7e1ba1ae
yhf2000 <735961159@qq.com>
2023-02-16 12:48:30 +0800

    fix: 增加 problemSet 表单提交的过滤

--- src/Component/problemSet/dataConvert/problemSetSubmit.ts
+++ src/Component/problemSet/dataConvert/problemSetSubmit.ts
@@ -1,4 +1,11 @@
 const problemSetSubmit = (data: any) => {
+    for (let x in data.config) {
+        if(x !== "practiceScoreCalculate" && x !== "practiceTimeSetting"){
+            if (data.config[x] === null) {
+                data.config[x] = 0
+            }
+        }
+    }
     return data
 }
 
