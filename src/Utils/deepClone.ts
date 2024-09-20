69dfa65ba3bfc546c6d62ab4750ca542a305700b
yhf2000 <735961159@qq.com>
2024-09-20 16:32:00 +0800

    fix: Add more conditions for deepClone

--- src/Utils/deepClone.ts
+++ src/Utils/deepClone.ts
@@ -1,8 +1,12 @@
-export default function deepClone(obj: any) {
+export default function deepClone(obj: any, hash = new WeakMap()) {
     const _toString = Object.prototype.toString;
     // null, undefined, non-object, function
     if (!obj || typeof obj !== 'object') {
         return obj;
+    }
+    // Handle circular reference
+    if (hash.has(obj)) {
+        return hash.get(obj);
     }
     // DOM Node
     if (obj.nodeType && 'cloneNode' in obj) {
@@ -28,8 +32,12 @@ export default function deepClone(obj: any) {
     }
     let result = Array.isArray(obj) ? [] :
         obj.constructor ? new obj.constructor() : {};
+
+    hash.set(obj, result);
     for (const key in obj) {
-        result[key] = deepClone(obj[key]);
+        if (obj.hasOwnProperty(key)) {
+            result[key] = deepClone(obj[key], hash);
+        }
     }
     return result;
 }
