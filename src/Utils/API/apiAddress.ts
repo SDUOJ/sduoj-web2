2d3cf6f82d4b2e109732beaeab39350e82b2c7f6
yhf2000 <hf_yin@qq.com>
2025-09-09 13:09:18 +0800

    fix: 开发端地址

--- src/Utils/API/apiAddress.ts
+++ src/Utils/API/apiAddress.ts
@@ -1,11 +1,11 @@
 export default function apiAddress() {
-    const port = "8889"
+    const port = "8080"
 
     if (process.env.NODE_ENV === 'development') return {
-        CLIENT_SERVER: 'http://api.test.sduoj.com:8889',
-        MANAGE_SERVER: 'http://api.test.sduoj.com:8889',
+        CLIENT_SERVER: 'http://api.test.sduoj.com:' + port,
+        MANAGE_SERVER: 'http://api.test.sduoj.com:'+ port,
         FRONT_SERVER: 'http://test.sduoj.com:3000',
-        SOCKET_SERVER: 'ws://api.test.sduoj.com:8889'
+        SOCKET_SERVER: 'ws://api.test.sduoj.com:' + port
     }
     return {
         CLIENT_SERVER: 'https://oj.qd.sdu.edu.cn',
